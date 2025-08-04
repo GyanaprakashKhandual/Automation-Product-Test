0d8db5dbdc76b99b5a6ec860b0d7aa238b989656
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-04 19:44:10 +0530

    Repo cleaned for testing my products

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/auth.controller');
-
-
-const router = express();
-
-router.post('/login', authController.login);
-
-module.exports = router;
