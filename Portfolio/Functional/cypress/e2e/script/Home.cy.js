8d5a269c699ad930d8ab09e4b6dea0577876a110
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-07 20:46:51 +0530

    Classic added

--- Portfolio/Functional/cypress/e2e/script/Home.cy.js
+++ Portfolio/Functional/cypress/e2e/script/Home.cy.js
@@ -8,4 +8,41 @@ describe('Home Page Tests', () => {
         appInstance.openApp();
     });
 
+    it('should navigate to the About page', () => {
+        appInstance.openAbout();
+    });
+
+    it('should navigate to the Education page', () => {
+        appInstance.openEduction();
+    });
+
+    it('should navigate to the Projects page', () => {
+        appInstance.openProjects();
+    });
+
+    it('should navigate to the Contact page', () => {
+        appInstance.openContact();
+    });
+
+    it('should navigate to the Blog page', () => {
+        appInstance.openBlog();
+    });
+
+    it('should navigate to the Work page', () => {
+        appInstance.openWork();
+    });
+
+    it('should navigate to the Development page', () => {
+        appInstance.openDevelopment();
+    });
+
+    it('should navigate to the Guides page', () => {
+        appInstance.openGuides();
+    });
+
+    it('should navigate to the Skills page', () => {
+        appInstance.openSkills();
+    });     
+    
+
 });
